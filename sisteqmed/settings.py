a0fe1f09d5063a3bcfb023eae6419124c7fbf399
edujtm <eduzemacedo@hotmail.com>
2018-11-25 18:04:35 -0300

    TOmar no cu

--- sisteqmed/settings.py
+++ sisteqmed/settings.py
@@ -98,7 +98,11 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
+<<<<<<< HEAD
 
+=======
+git
+>>>>>>> TOmar no cu
 DATABASES['default'].update(db_from_env)
 
 # Password validation
